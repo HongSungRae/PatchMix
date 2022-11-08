49be66379d06c260be3eb21af040dcedf20ec5ad
HongSungRae <sr5043@naver.com>
2022-11-08 12:53:28 +0900

    빌딩중

---
+++ datasets/digestpath19_preprocessing.py
@@ -0,0 +1,13 @@
+# library
+import pandas as pd
+
+# local
+from parameters import digestpath19_split_info
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
--- parameters.py
+++ parameters.py
@@ -1,1 +1,9 @@
 SEED = 111
+digestpath19_split_info = {
+                           'train_M' : 350,
+                           'train_N' : 288,
+                           'validation_M' : 75,
+                           'validation_N' : 61,
+                           'test_M' : 75,
+                           'test_N' : 61
+                           }
