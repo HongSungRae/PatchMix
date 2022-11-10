093f88b15b2bfefb617843ece451317036a76972
HongSungRae <sr5043@naver.com>
2022-11-10 14:51:02 +0900

    digestpath19 dataset 파일 만듦

--- datasets/digestpath19.py
+++ datasets/digestpath19.py
@@ -8,11 +8,11 @@ import torch.nn as nn
 
 
 class DigestPath19(nn.Module):
-    def __init__(self, split='train', dataset='NM', augmentation=None, aug_p=0.5, size=256):
+    def __init__(self, split='train', dataset='NM', augmentation=None, aug_p=0.5, ratio=None, size=256):
         super().__init__()
-        assert split in ['train', 'test', 'validation'], "BCNB split Error"
-        assert dataset in ['M','NM'], "BCNB dataset Error"
-        assert augmentation in ['cp_simple', 'cp_gaussian', 'cp_poisson', 'cp_tumor', 'cutmix_half', 'cutmix_dice', 'cutmix_random', 'image_aug', None], "BCNB augmentation Error"
+        assert split in ['train', 'test', 'validation'], "DigestPath19 split Error"
+        assert dataset in ['M','NM'], "DigestPath19 dataset Error"
+        assert augmentation in ['cp_simple', 'cp_gaussian', 'cp_poisson', 'cp_tumor', 'cutmix_half', 'cutmix_dice', 'cutmix_random', 'image_aug', None], "DigestPath19 augmentation Error"
 
 
     def __len__(self):
