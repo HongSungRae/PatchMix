eb22983bd582ce9c1dde106dddf87615b0eb5c44
HongSungRae <sr5043@naver.com>
2022-11-11 16:14:36 +0900

    split 정보 수정

--- parameters.py
+++ parameters.py
@@ -1,9 +1,9 @@
 SEED = 111
 digestpath19_split_info = {
-                           'train_M' : 350,
+                           'train_M' : 175,
                            'train_N' : 288,
-                           'validation_M' : 75,
+                           'validation_M' : 37,
                            'validation_N' : 61,
-                           'test_M' : 75,
+                           'test_M' : 38,
                            'test_N' : 61
                            }
